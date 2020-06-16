cad3e3749574a24f7e66966e9a18ff17f26d5ed1
VedanshGoenka <vedansh@vgoenka.com>
2020-06-15 19:45:40 -0700

    Limit the number of events to show as the first param, default to 10

--- showevents.sh
+++ showevents.sh
@@ -2,9 +2,12 @@
 
 showEvents()
 {
-	echo "select e.eventId, e.startTime, e.endTime, e.startTimeMillis, e.endTimeMillis, z.name from Events e join Zones z on e.zoneId = z.zoneId;" |
-	/usr/bin/sqlite3 data/sentryDB.sqlite3
+	echo "select e.eventId, e.startTime, e.endTime, e.startTimeMillis, e.endTimeMillis, z.name from Events e join Zones z on e.zoneId = z.zoneId ORDER BY e.startTime DESC LIMIT $MAXLINES;" |
+	/usr/bin/sqlite3 data/sentryDB.sqlite3 |
+	sort 
 }
+
+MAXLINES=${1:-10}
 
 while [ 1 ]
 do
